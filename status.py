a3453e1b3b916e4a9c5dd35e290caaf59e6427b7
recvmsgs <105562133+recvmsgs@users.noreply.github.com>
2022-05-16 14:14:56 +0200

    Update status.py

--- status.py
+++ status.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-#
+
 import os,  sys, re, requests, smtplib, ssl
-#
+
 url = 'https://status.digitalocean.com/api/v2/summary.json'
-#
+
 smtp_server = os.environ.get('SMTP_ADDR','smtp.gmail.com')
 port = os.environ.get('SMTP_PORT',587)
 sender_email = os.environ.get('MAIL_FROM','skit.alerts@gmail.com')
 receiver_email = os.environ.get('SKIT_ALERT_EMAIL','recvmsgs@gmail.com')
 password = os.environ.get('MAIL_PASS','nepoviemHesl0')
-#
+
 subject = 'ISSUES, see https://status.digitalocean.com/'
 text = subject
 message = 'Subject: {}\n\n{}'.format(subject, text)
-#
+
 def status():
     r = requests.get(url)
     if re.search('\"scheduled_maintenances\":\[\],', r.text):
@@ -33,6 +33,6 @@ def run():
     else:
         print("ISSUES")
         sendmail()
-#
+
 run()
-#
+
