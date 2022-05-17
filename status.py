959fbfc8f8011d0db357a7ef72df82d8bcf11a23
recvmsgs <105562133+recvmsgs@users.noreply.github.com>
2022-05-17 16:51:56 +0200

    Update status.py

--- status.py
+++ status.py
@@ -10,29 +10,35 @@ sender_email = os.environ.get('MAIL_FROM','skit.alerts@gmail.com')
 receiver_email = os.environ.get('SKIT_ALERT_EMAIL','recvmsgs@gmail.com')
 password = os.environ.get('MAIL_PASS','nepoviemHesl0')
 
-subject = 'ISSUES, see https://status.digitalocean.com/'
+subject = 'ISSUES! See https://status.digitalocean.com/'
 text = subject
 message = 'Subject: {}\n\n{}'.format(subject, text)
 
 def status():
     r = requests.get(url)
-    if re.search('\"scheduled_maintenances\":\[\],', r.text):
-        if re.search('"incidents":\[\],', r.text):
-            if re.search('"status":"operational"', r.text):
+    if '"scheduled_maintenances":[],' in r.text:
+        print ( '"scheduled_maintenances":[],' in r.text)
+        if  '"incidents":[],' in r.text:
+            print( '"incidents":[],' in r.text)
+            if '"status":"operational"' in r.text:
+                print('"status":"operational"' in r.text)
                 return True
+
 def sendmail():
     context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port, timeout=3500) as server:
+    with smtplib.SMTP(smtp_server, port, timeout=3) as server:
         server.starttls(context=context)
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
-        print("EMAIL SENT")
+        print("EMAIL SENT:\n"  + sender_email + '\n' + receiver_email + '\n' + password + '\n' + smtp_server + ':' + port + '\n')
+
 def run():
     if status():
         print("OK")
     else:
-        print("ISSUES")
+        print("ISSUES!")
         sendmail()
 
 run()
+
 
